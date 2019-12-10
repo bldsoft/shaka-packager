ea16e724030657f9d7c4723ff41fa6023b3ae92e
Sergey Yarosh <s.yarosh@setplex.com>
2019-12-10 18:04:53 +0300

    do not use custom allocator

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-GYP_DEFINES='clang=0 libpackager_type=shared_library' gclient runhooks
+GYP_DEFINES='clang=0 use_experimental_allocator_shim=0 use_allocator=none libpackager_type=shared_library' gclient runhooks
 ninja -C out/Release
-ninja -C out/Debug
