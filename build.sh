d34fe7bdcc1c89990f5404229b765660eda39819
Sergey Gorelyshev <s.gorelyshev@setplex.com>
2020-12-09 23:49:30 +0300

    tmp

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-GYP_DEFINES='clang=0 use_experimental_allocator_shim=0 use_allocator=none' gclient runhooks
+GYP_DEFINES='clang=0 use_experimental_allocator_shim=0 use_allocator=none --args=is_debug=true' gclient runhooks
 ninja -C out/Release
 ninja -C out/Debug
