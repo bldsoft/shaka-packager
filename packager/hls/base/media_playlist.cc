e3c191b4c8784f7fe03666c1421a2eedf4e7f26c
Sergey Gorelyshev <s.gorelyshev@setplex.com>
2021-07-13 14:19:23 +0000

    [STR-931] make video width even in hls playlists

--- packager/hls/base/media_playlist.cc
+++ packager/hls/base/media_playlist.cc
@@ -574,8 +574,12 @@ bool MediaPlaylist::GetDisplayResolution(uint32_t* width,
             ? static_cast<double>(media_info_.video_info().pixel_width()) /
                   media_info_.video_info().pixel_height()
             : 1.0;
-    *width = static_cast<uint32_t>(media_info_.video_info().width() *
-                                   pixel_aspect_ratio);
+
+    uint32_t nearest_width = static_cast<uint32_t>(
+        std::nearbyint(media_info_.video_info().width() * pixel_aspect_ratio));
+
+    // Avoiding odd width
+    *width = nearest_width % 2 == 0 ? nearest_width : nearest_width + 1;
     *height = media_info_.video_info().height();
     return true;
   }
